194d989e8c2aa496a650b64e7c20f2cf32655236
n4b3l <dendende@hotmail.com>
2018-12-06 21:45:22 +0100

    shenanigans

--- resources/views/pages/view_challenge.blade.php
+++ resources/views/pages/view_challenge.blade.php
@@ -9,24 +9,22 @@
 @endsection
 @section('content')
     <?php
-    $scores = DB::table('challenge_user_tt')->where('challenge_id',$id)->get();
+    $scores = DB::table('challenge_user_tt')->where('challenge_id',$id)->orderBy('score', 'desc')->orderBy('beatmap_id', 'asc')->get();
     ?>
     <table id="challengeTable" class="table  table-bordered" style="width:100%">
         <thead>
         <tr>
-            <th scope="col">Id</th>
             <th scope="col">Title</th>
             <th scope="col">Player</th>
             <th scope="col">Score</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody class="hoverable">
         <?php
         foreach ($scores as $score) { ?>
         <tr>
-            <th scope="row"><?= $score->beatmap_id ?></th>
-            <td><?= $score->beatmap_name ?></td>
-            <td><?= $score->user_id ?></td>
+            <td class='clickable-row' data-href='https://osu.ppy.sh/b/<?= $score->beatmap_id   ?>'><?= $score->beatmap_name ?></td>
+            <td class='clickable-row' data-href='https://osu.ppy.sh/u/<?= $score->user_id   ?>'><?= $score->user_id ?></td>
             <td><?= $score->score ?></td>
         </tr>
         <?php
@@ -45,8 +43,18 @@
             $("tr:odd").css({
                 "background-color": "white",
                 "color": "deeppink"});
-            $(".clickable-row").click(function() {
-                window.location = $(this).data("href");
+            $(".clickable-row").click(function(e) {
+                if (e.ctrlKey || e.which == 2 || e.button == 4) {
+                    window.open($(this).data("href"));
+                }
+                else {
+                    window.location = $(this).data("href");
+                }
+            });
+            $(".clickable-row").mousedown(function(e) {
+                if( e.which == 2 ) {
+                    window.open($(this).data("href"));
+                }
             });
         });
         // $(document).ready( function () {
